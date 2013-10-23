d80e4452508f4c25d27f7dbe978b19fe71c23e3d
Ted Schmidt <ted@sense-os.nl>
2013-10-23 10:40:19 +0200

    updated geofence and carry device demo

--- android/src/nl/sense/demo/cortex/presence/CarryDeviceDemo.java
+++ android/src/nl/sense/demo/cortex/presence/CarryDeviceDemo.java
@@ -40,7 +40,7 @@ public class CarryDeviceDemo {
 			// Create new GetData DataProcessor which is used to display the data on a fragment, and send it to CommonSense
 			getData = new GetData(FragmentDisplay.newInstance(TAG));
 			// Create the actual CarryDevice DataProcessor, which will be registered at the Sense Service with the given name (TAG)
-			carryDevice = new CarryDeviceSensor(TAG, sensePlatform.getService().getSenseService());
+			carryDevice = new CarryDeviceSensor(TAG, sensePlatform);
 			// This resets the learned noise values, when erroneous data with no variance is processed
 			// the lowest variance used to determine the noise is 0 which means that the smallest change will cause an event
 			//carryDevice.reCalibrate();
