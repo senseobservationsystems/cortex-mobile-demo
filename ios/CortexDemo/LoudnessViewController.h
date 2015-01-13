7f6c14517ab1ce0ec7aaef9ae4c97d6db173fc8f
Joris Janssen <joris@sense-os.nl>
2015-01-13 09:43:13 +0100

    Added pages for for loudness test and persistency of local data

---
+++ ios/CortexDemo/LoudnessViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoudnessViewController.h
+//  CortexDemo
+//
+//  Created by Joris Janssen on 12/01/15.
+//  Copyright (c) 2015 Sense Observation Systems BV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoudnessViewController : UIViewController
+
+@end
